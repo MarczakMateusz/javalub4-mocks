b54359b14b6e2db8deb2516224fc2e8766881687
MarczakMateusz <Mateusz.Szymon.Marczak@gmail.com>
2017-12-11 19:22:02 +0100

    Commit nr 1

--- src/main/java/com/demo/camera/PhotoCamera.java
+++ src/main/java/com/demo/camera/PhotoCamera.java
@@ -1,17 +1,55 @@
 package com.demo.camera;
 
-public class PhotoCamera {
+public class PhotoCamera implements WriteListener {
+    private boolean cameraState;
+    private ImageSensor sensor;
+    private Card card;
+    private Boolean writeCompleted;
+
+    public PhotoCamera(ImageSensor sensor,Card card) {
+        this.cameraState = false;
+        this.sensor = sensor;
+        this.card = card;
+    }
+
+    public PhotoCamera(ImageSensor sensor) {
+        this.cameraState = false;
+        this.sensor = sensor;
+    }
+
+    public PhotoCamera( Card card) {
+        this.cameraState = false;
+        this.card = card;
+    }
+
+    public boolean isCameraState() {
+        return cameraState;
+    }
+
 
     public void turnOn() {
-        // not implemented
+        cameraState = true;
+        sensor.turnOn();
+
     }
 
     public void turnOff() {
-        // not implemented
+        cameraState = true;
+        sensor.turnOff();
     }
 
     public void pressButton() {
-        // not implemented
+        if(cameraState){
+
+
+            card.write(sensor.read());
+        }
+    }
+
+    @Override
+    public void writeCompleted() {
+
+
     }
 }
 
--- src/test/java/com/demo/camera/PhotoCameraSpec.groovy
+++ src/test/java/com/demo/camera/PhotoCameraSpec.groovy
@@ -6,6 +6,58 @@ class PhotoCameraSpec extends Specification {
 
 
     def "Should power up the sensor when camera is switched on"() {
+        given:
+        ImageSensor testSensor = Mock(ImageSensor)
+        PhotoCamera testCamera = new PhotoCamera(testSensor)
+
+        when:
+        testCamera.turnOn()
+
+        then:
+        1 * testSensor.turnOn()
+
+    }
+
+    def "Should power down sensor when camera is switched off"(){
+        given:
+        ImageSensor testSensor = Mock(ImageSensor)
+        PhotoCamera testCamera = new PhotoCamera(testSensor)
+
+        when:
+        testCamera.turnOff()
+
+        then:
+        1 * testSensor.turnOff()
+
+    }
+
+    def "Pushing the button when camera is Off should do nothing"(){
+        given:
+        ImageSensor testSensor = Mock(ImageSensor)
+        Card testCard = Mock(Card)
+        PhotoCamera testCamera = new PhotoCamera(testSensor,testCard)
+
+        when:
+        testCamera.pressButton()
+
+        then:
+        0 * testSensor.turnOff()
+        0 * testSensor.turnOn()
+        0 * testSensor.read()
+        0 * testCard.write()
+    }
+    def "Pressing the burron when camera is on should copy date from sensor to card"(){
+        given:
+        ImageSensor testSensor = Mock(ImageSensor)
+        Card testCard = Mock(Card)
+        PhotoCamera testCamera = new PhotoCamera(testSensor,testCard)
+
+        when:
+        testCamera.pressButton()
+
+        then:
+        1 * testSensor.read()
+        1 * testCard.write()
 
     }
 
